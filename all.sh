bf876d62343a2688df921ddd9924abbb0066d12d
xianghuy <yexianghu@gmail.com>
2015-06-12 18:37:47 +0800

    first commit

---
+++ all.sh
@@ -0,0 +1,22 @@
+###------------------------------------   git setting begin ----------------------------------###
+
+#Identity
+git config --global user.name "xianghuy"
+git config --global user.email yexianghu@gmail.com
+
+#editor
+git config --global core.editor vim
+
+#color ui
+git config --global color.ui true
+
+#color whitespace and table
+git config --global core.whitespace trailing-space,space-before-tab,indent-with-non-tab
+
+# alias
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.co checkout
+git config --global alias.ci commit
+
+###------------------------------------   git setting end ----------------------------------###
---
+++ install.sh
@@ -0,0 +1,58 @@
+#insall gnome optional
+#sudo apt-get install gnome
+
+#PPA for synapse
+sudo apt-add-repository -y ppa:synapse-core/ppa 
+
+#PPA for android studio
+sudo apt-add-repository -y ppa:paolorotolo/android-studio
+
+#PPA for oracle java
+sudo add-apt-repository -y ppa:webupd8team/java 
+
+
+
+sudo apt-get update
+
+
+
+#install common tools
+sudo apt-get install -y vim guake emacs24 synapse gconf-editor git subversion gcolor2 kolourpaint4 virtualbox
+
+#install develop tools
+sudo apt-get install -y android-studio oracle-java7-installer
+
+#chnroute setup
+# https://github.com/fivesheep/chnroutes
+
+#download skype
+# http://www.skype.com
+
+
+# create github folder
+mkdir -p ~/github
+
+# download bash config from github
+mkdir -p ~/.bash.d
+cd ~/github
+git clone https://github.com/yexianghu/yezi_bash.git
+ln -s ~/github/yezi_bash .bash.d
+echo "source ~/.bash.d/all.sh" >> ~/.bashrc
+
+
+#download emacs configure from github
+cd ~/github
+git clone https://github.com/yexianghu/yezi_emacs_configure.git
+cd ~/github/yezi_emacs_configure
+git checkout origin/new_dev
+ln -s ~/github/yezi_emacs_configure ~/.emacs.d
+
+
+#download bcompare
+# http://www.scootersoftware.com/download.php
+
+
+
+
+
+
